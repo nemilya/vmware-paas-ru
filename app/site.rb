8ee81b23596d7a0cafddb56e8de3e395d094a58a
nemilya <Ilya Nemihin@.(none)>
2011-04-22 16:26:34 +0600

    refactoring

--- app/site.rb
+++ app/site.rb
@@ -1,6 +1,11 @@
 require 'rubygems'
 require 'sinatra'
 
+$:.unshift( File.dirname(__FILE__))
+
+set :public, File.join( File.dirname(__FILE__), '../public' )
+set :views, File.join( File.dirname(__FILE__), '../views' )
+
 require 'before' # init for @articles
 require 'helpers'
             
@@ -11,11 +16,11 @@ end
 get '/cloud-foundry-open-paas-deep-dive' do
   @a_id = 'cloud-foundry-open-paas-deep-dive'
   @article = get_article
-  erb @a_id.to_sym
+  erb "articles/#{@a_id}".to_sym
 end
 
 get '/what-happens-when-you-vmc-push-an-application-to-cloud' do
   @a_id = 'what-happens-when-you-vmc-push-an-application-to-cloud'
   @article = get_article
-  erb @a_id.to_sym
+  erb "articles/#{@a_id}".to_sym
 end
--- config.ru
+++ config.ru
@@ -1,2 +1,2 @@
-require 'site'
+require 'app/site'
 run Sinatra::Application
